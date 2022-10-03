c8ef48c3c0a79214a0263e96faf8fda2415f541b
HomoHikka <halfideas31@gmail.com>
2022-10-04 00:30:39 +0300

    Update Utilities.ts
    - add random number/range funtions
    - add Bounds class to calculate local bounds of node

--- assets/scripts/Utilities.ts
+++ assets/scripts/Utilities.ts
@@ -1,3 +1,5 @@
+import { Node, UITransform, Vec2, Vec3 } from 'cc';
+
 export function randomEnumKey<T extends object>(anEnum: T, limitTo?: number): T[keyof T] {
 	const values = Object.keys(anEnum)
 		.map(n => Number.parseInt(n))
@@ -9,8 +11,8 @@ export function randomEnumKey<T extends object>(anEnum: T, limitTo?: number): T[
 
 export type EnumType<T> = T[keyof T];
 
-export function shuffleArray(array: Array<unknown>) {
-	let currentIndex = array.length, randomIndex;
+export function shuffleArray(array: Array<unknown>): Array<unknown> {
+	let currentIndex = array.length, randomIndex: number;
 
 	while (currentIndex != 0) {
 
@@ -21,4 +23,50 @@ export function shuffleArray(array: Array<unknown>) {
 	}
 
 	return array;
+}
+
+export class Bounds {
+	readonly topLeft: Vec2;
+	readonly bottomRight: Vec2;
+	readonly width: number;
+	readonly height: number;
+
+	constructor(node: Node) {
+		const { x, y } = node.position;
+		const transform = node.getComponent(UITransform) as UITransform;
+
+		const width = (transform?.width ?? 0) * node.scale.x;
+		const height = (transform?.height ?? 0) * node.scale.y;
+
+		const anchorX = transform?.anchorX ?? 0.5;
+		const anchorY = transform?.anchorY ?? 0.5;
+
+		this.topLeft = new Vec2(x - width * anchorX, y + height * (1 - anchorY));
+		this.bottomRight = new Vec2(x + width * (1 - anchorX), y - height * anchorY);
+		this.width = width;
+		this.height = height;
+	}
+
+	public getRandomPosition(): Vec3 {
+		return new Vec3(
+			this.topLeft.x + randomFloat(this.width),
+			this.bottomRight.y + randomFloat(this.height)
+		);
+	}
+}
+
+export function randomFloat(max: number): number {
+	return Math.random() * max;
+}
+
+export function randomRange(min: number, max: number): number {
+	return min + Math.random() * (max - min);
+}
+
+export function randomInt(max: number): number {
+	return Math.floor(Math.random() * max);
+}
+
+export function randomIntRange(min: number, max: number): number {
+	return Math.floor(randomRange(min, max));
 }
