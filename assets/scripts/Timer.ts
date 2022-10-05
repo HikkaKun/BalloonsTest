52bbabdb245c3e31cd572aea71956d31501023a6
HomoHikka <halfideas31@gmail.com>
2022-10-05 14:04:00 +0300

    Update Timer.ts
    - now interval can be changed

--- assets/scripts/Timer.ts
+++ assets/scripts/Timer.ts
@@ -1,12 +1,13 @@
 export default class Timer {
 	private _time = 0;
 	private _isStarted = false;
-	private readonly _interval;
+
+	public interval = 1;
 
 	public OnTimerEndCallback: () => void;
 
-	constructor(interval, isStart = false) {
-		this._interval = interval;
+	constructor(interval: number, isStart = false) {
+		this.interval = interval;
 
 		isStart && this.start();
 	}
@@ -19,7 +20,7 @@ export default class Timer {
 		if (!this._isStarted) return;
 
 		this._time += dt;
-		if (this._time >= this._interval) {
+		if (this._time >= this.interval) {
 			this._time = 0;
 			this.OnTimerEndCallback instanceof Function && this.OnTimerEndCallback();
 		}
