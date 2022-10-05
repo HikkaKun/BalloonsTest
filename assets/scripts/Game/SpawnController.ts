5f349d3daa250172f648a91f4b89b3755468423e
HomoHikka <halfideas31@gmail.com>
2022-10-05 14:05:04 +0300

    Create SpawnController
    - create SpawnController to control BalloonSpawner

---
+++ assets/scripts/Game/SpawnController.ts
@@ -0,0 +1,39 @@
+
+import { _decorator, Component, Node } from 'cc';
+import { GameState } from '../Plugins/Game/GameState';
+import { GameEvent } from '../Plugins/GameEvent';
+import GlobalEvent from '../Plugins/GlobalEvent';
+import { BalloonSpawner } from './BalloonSpawner';
+const { ccclass, property } = _decorator;
+
+@ccclass('SpawnController')
+export class SpawnController extends Component {
+	@property(BalloonSpawner)
+	spawner: BalloonSpawner;
+
+	protected onEnable() {
+		this._handleEvents(true);
+	}
+
+	protected onDisable() {
+		this._handleEvents(false);
+	}
+
+	protected _handleEvents(isOn: boolean) {
+		const func = isOn ? 'on' : 'off';
+
+		GlobalEvent[func](GameEvent.ChangeGameState, this.OnChangeGameState, this);
+	}
+
+	public OnChangeGameState(gameState: number) {
+		if (!this.spawner) return;
+
+		switch (gameState) {
+			case GameState.Game:
+				this.spawner.reset();
+				break;
+			case GameState.Lose:
+				this.spawner.stop();
+		}
+	}
+}
